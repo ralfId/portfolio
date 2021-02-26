7d784d03d2beda5f738845c10902a00c12273f2b
Rafael Ibañez <ralf_raid@outlook.es>
2021-02-26 02:23:48 -0600

    first commit to m digital repository

---
+++ js/main.js
@@ -0,0 +1,71 @@
+(function(){
+    "use strict";
+
+    /**
+     * --------TODO
+     * 
+     * WHEN FINISH TEST ALL THIS FUNCTIONS FOR REPONSIBE MOBILE MENU
+     */
+    /**easy selector helper function*/
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        }else{
+            return document.querySelector(el)
+        }
+    }
+
+    /**easy event listener funcion*/
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            }else {
+                selectEl.addEventListener(type, listener)
+            }
+        } 
+    }
+
+    /**easy on scroll event listener */
+    const onscroll = (el, listener) => { el.addEventListener('scroll', listener)}
+
+    /**navbar links active on scroll */
+    let navbarlinks  = select('#navbar .scrollto', true)
+    
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+
+            let section = select(navbarlink.hash)
+
+            if (!section) return
+
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            }else{ navbarlink.classList.remove('active')}
+        })
+    }
+
+    window.addEventListener('load', navbarlinks)
+    onscroll(document, navbarlinksActive)
+
+    /**scrolls to an element with header offset */
+    const scrollto = (el) => {
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos,
+            behavior: 'smooth'
+        })
+    }
+
+    /**mobile nav toggle */
+on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+})()
